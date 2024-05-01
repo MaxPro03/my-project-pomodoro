453b6375a3c821dfde70657eb614b857d22d898d
unknown <pumpkinmaxpro@gmail.com>
2024-05-01 23:53:28 +0500

    feat: change base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://maxpro03.github.io/my-project-pomodoro/',
+  base: '/',
   plugins: [
     vue(),
     VitePWA({
