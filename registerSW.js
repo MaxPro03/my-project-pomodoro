180e7de11086bcb67f8bfe7a6c810524e1b457a6
unknown <pumpkinmaxpro@gmail.com>
2024-05-01 23:40:54 +0500

    feat: change base url

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://maxpro03.github.io/my-project-pomodoro/sw.js', { scope: 'https://maxpro03.github.io/my-project-pomodoro/' })})}
