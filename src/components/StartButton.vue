54ec11b6ad1b7ec2b4ad147a62c5c92eff8f9d41
@m.prokopenko <m.prokopenko@uzum.com>
2026-09-02 18:43:49 +0500

    test changes 2

--- src/components/StartButton.vue
+++ src/components/StartButton.vue
@@ -12,7 +12,7 @@
       class="relative z-10 mb-4 w-full bg-orange-500 px-4 py-2 uppercase text-white before:absolute before:inset-y-2.5 before:-inset-x-2.5 before:-z-10 before:block before:bg-orange-500 before:content-[''] after:absolute after:inset-y-1 after:-inset-x-1.5 after:-z-10 after:block after:bg-orange-500 after:content-[''] md:mx-auto md:w-40"
       @click="timerStore.startTimerStore(timerData)"
     >
-      Star
+      Start
     </button>
     <button
       v-if="timerStore.timerReset"
